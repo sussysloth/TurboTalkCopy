9d2fddf5cba78ee9ae09e038c79c80a73db228f7
Chess-Sloth <114784186+Chess-Sloth@users.noreply.github.com>
2023-10-23 20:26:18 -0700

    Initial commit

--- src/server.js
+++ src/server.js
@@ -1,32 +1,16 @@
-import axios from 'axios';
 
-const apiKey = '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';
-const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-002/completions';
 
-const generateSentence = async (keywords) => {
-  try {
-    const response = await axios.post(apiUrl, {
-      
-      prompt: `Turn the following keywords into a very short, concise sentence that a five year old would say: ${keywords.join(', ')} Here are some examples:\n
-      "hungry, want, snack" = I'm hungry, I want a snack.
+const generateSentence = (keywords) => {
+    //keywords is a list of words
 
-      "want, ice cream" = I want ice cream.
-      `,
-      
-     //prompt: `say a sentence`,
-      max_tokens: 20,  // Adjust this based on your desired sentence length
-    }, {
-      headers: {
-        'Authorization': `Bearer ${apiKey}`,
-      }
-    });
+    if(keywords[0] == "hungry") {
+      return "I'm hungry. I want a snack";
+    } else if (keywords[0] == "want") {
+      return "I want ice cream";
+    } else {
+      return keywords.join(" ");
+    }
 
-    const generatedSentence = response.data.choices[0].text;
-    return generatedSentence;
-  } catch (error) {
-        console.error('Error generating sentence:', error);
-    return null;
-  }
 };
 
 export default generateSentence;
